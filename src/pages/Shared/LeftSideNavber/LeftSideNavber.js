c70e2119608d94d0a3cfc87819f697fb8e421ca3
mzhdipu <mojammelhossaindipu@gmail.com>
2022-10-25 21:02:31 +0600

    install all require packages & completed homepage design

---
+++ src/pages/Shared/LeftSideNavber/LeftSideNavber.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftSideNavber = () => {
+    return (
+        <div>
+            <h1>Left Side Navber</h1>
+        </div>
+    );
+};
+
+export default LeftSideNavber;
