5f32fc8d133677f3bb15a6ab4b0aec8f1d0a02cb
mzhdipu <mojammelhossaindipu@gmail.com>
2022-10-27 22:49:41 +0600

    updated not found page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Page Not Found</h1>
-            <Button variant="primary"><Link to="/">Go to Home</Link></Button>{' '}
-        </div>
+        <Container>
+            <Row className='d-flex justify-content-center align-items-center text-center' style={{height: "100vh"}}>
+                <div>
+                    <h1>Page Not Found</h1>
+                    <Button variant="primary"><Link to="/" className='text-white text-decoration-none fw-bold'>Go to Home</Link></Button>{' '}
+                </div>
+            </Row>
+        </Container>
     );
 };
 
